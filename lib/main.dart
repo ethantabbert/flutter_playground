3bc8edd511c9fe1cd258d136533d8fca71ec25f6
Ethan Tabbert <etabbert@vermeer.com>
2024-04-02 12:22:19 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/sidebar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Sidebar();
+  }
+}
---
+++ lib/sidebar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatefulWidget {
+  const Sidebar({super.key});
+
+  @override
+  SidebarState createState() => SidebarState();
+}
+
+class SidebarState extends State<Sidebar> with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  bool isExtended = false;
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    animation = Tween<double>(begin: 1.0, end: 0.0).animate(controller);
+  }
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _toggleBar() {
+    setState(() {
+      isExtended = !isExtended;
+    });
+    if (isExtended) {
+      controller.forward();
+    } else {
+      controller.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+        bottomNavigationBar: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () => _toggleBar(),
+              icon: AnimatedIcon(
+                icon: AnimatedIcons.close_menu,
+                progress: animation,
+              ),
+            ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              height: isExtended ? (screenHeight / 8) : 0,
+              alignment: Alignment.center,
+              child: BottomAppBar(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.home),
+                      onPressed: () => _onTap(0),
+                      color: _selectedIndex == 0 ? Colors.blue : Colors.grey,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () => _onTap(1),
+                      color: _selectedIndex == 1 ? Colors.blue : Colors.grey,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.notifications),
+                      onPressed: () => _onTap(2),
+                      color: _selectedIndex == 2 ? Colors.blue : Colors.grey,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.account_circle),
+                      onPressed: () => _onTap(3),
+                      color: _selectedIndex == 3 ? Colors.blue : Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
